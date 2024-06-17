93c93a1cc52755f6eaf2be64816e91add9bcca34
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-17 09:56:11 -0700

    Revert "Restore PatchCore."
    This reverts commit 7002b363f3e9b256cbe17a114200e28ab944dacb.

--- predict.sh
+++ predict.sh
@@ -1,1 +1,1 @@
-anomalib predict --return_predictions false --ckpt_path results/EfficientAd/avatar_rigging/v8/weights/lightning/model.ckpt --config avatar_rigging.yaml --model Patchcore
+anomalib predict --return_predictions false --ckpt_path results/EfficientAd/avatar_rigging/v8/weights/lightning/model.ckpt --config avatar_rigging.yaml
