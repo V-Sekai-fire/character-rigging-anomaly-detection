728ee675da1bb247f0b43570726e9c2b43d91e7e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-17 05:36:47 -0700

    Move the folders for organization. Also switch to EfficientAd.
    Remove pretrained.

--- predict.sh
+++ predict.sh
@@ -1,1 +1,1 @@
-anomalib predict --return_predictions false --ckpt_path results/Patchcore/avatar_rigging/latest/weights/lightning/model.ckpt --model anomalib.models.Patchcore --config avatar_rigging.yaml
+anomalib predict --return_predictions false --ckpt_path results/EfficientAd/avatar_rigging/latest/weights/lightning/model.ckpt --model anomalib.models.EfficientAd --config avatar_rigging.yaml
