3c7b9d38731162b93b211f4ce82fd3107a67ab6b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-18 21:40:27 -0700

    Updated image size parameter in training function

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from cog import Path
 import io
 
 def train(param: str) -> Path:
-    datamodule = Folder(name="hazelnut_toy", normal_dir="good", root="datasets/hazelnut_toy", abnormal_dir=None, normal_test_dir=None, mask_dir=None, normal_split_ratio=0.2, extensions=None, train_batch_size=1, eval_batch_size=32, num_workers=8, task=TaskType.SEGMENTATION, image_size=(1080, 1920), transform=None, train_transform=None, eval_transform=None, test_split_mode=TestSplitMode.SYNTHETIC, test_split_ratio=0.2, val_split_mode=ValSplitMode.FROM_TEST, val_split_ratio=0.5, seed=None)
+    datamodule = Folder(name="hazelnut_toy", normal_dir="good", root="datasets/hazelnut_toy", abnormal_dir=None, normal_test_dir=None, mask_dir=None, normal_split_ratio=0.2, extensions=None, train_batch_size=1, eval_batch_size=32, num_workers=8, task=TaskType.SEGMENTATION, image_size=None, transform=None, train_transform=None, eval_transform=None, test_split_mode=TestSplitMode.SYNTHETIC, test_split_ratio=0.2, val_split_mode=ValSplitMode.FROM_TEST, val_split_ratio=0.5, seed=None)
     datamodule.setup()
     model = EfficientAd(imagenet_dir="datasets/hazelnut_toy", teacher_out_channels=384, model_size=EfficientAdModelSize.S, lr=0.0001, weight_decay=1e-05, padding=False, pad_maps=True)
     engine = Engine()
